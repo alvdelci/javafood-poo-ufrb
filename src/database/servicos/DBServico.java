d914478625302d1a548cc40da1bb7077c14341e6
alvdelci <delcimariosantos@gmail.com>
2021-09-18 17:04:33 -0300

    Adicionando superclasse de database de servicos

---
+++ src/database/servicos/DBServico.java
@@ -0,0 +1,16 @@
+package database.servicos;
+
+import controllers.servicos.Servico;
+
+public class DBServico {
+    // Construtor sem argumentos, apenas para criação de objetos
+    public DBServico() {
+    }
+
+    // Construtor de 1 argumento
+    public DBServico(Servico produto) {
+    }
+
+    // Recebe um objeto do tipo Servico e adiciona à lista de Produtos
+    public void adicionarProduto(Servico produto){};
+}
