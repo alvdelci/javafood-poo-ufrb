161c165a1f8bf225f6429124350fc9cb34a993c0
alvdelci <delcimariosantos@gmail.com>
2021-09-23 12:04:09 -0300

    Criando database de Usuario

---
+++ src/database/usuario/DBUsuario.java
@@ -0,0 +1,35 @@
+package database.usuario;
+
+import java.util.ArrayList;
+
+import controllers.usuarios.Usuario;
+
+public class DBUsuario {
+
+    // ArrayList armazena todos os Usuario cadastrados
+    private ArrayList<Usuario> usuarioList = new ArrayList<Usuario>();
+
+    //Usuário administrador pré cadastrado
+    Usuario usuarioA = new Usuario("Administrador", "admin", "00000000000", "Raccon", "admin@mail.com", "admin");
+    // Dados de pré cadastro
+    Usuario usuario = new Usuario("Rafithy", "Haspy", "11111111111", "Cruz das Almas", "rafy@mail.com", "batata");
+    Usuario usuario1 = new Usuario("Tony", "Stark", "22222222222", "Los Angeles", "starkin@mail.com", "imironman");
+    Usuario usuario2 = new Usuario("Wanda", "Vision", "33333333333", "Texas", "vision@mail.com", "scarlet");
+
+    // Construtor sem argumentos, apenas para criação de objetos
+    public DBUsuario() {
+        adicionarUsuario(usuario);
+        adicionarUsuario(usuario1);
+        adicionarUsuario(usuario2);
+    }
+
+    // Recebe um objeto do tipo Usuario e adiciona à lista de usuarios
+    public void adicionarUsuario(Usuario usuario) {
+        usuarioList.add(usuario);
+    }
+
+    // Retorna todos os usuarios cadastrados no array de usuarios
+    public ArrayList<Usuario> returnUsuario() {
+        return usuarioList;
+    }
+}
