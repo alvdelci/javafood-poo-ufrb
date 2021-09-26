b7a1c726def3b6f2138365569e17c40ecb8e1be4
alvdelci <delcimariosantos@gmail.com>
2021-09-26 14:57:48 -0300

    Adicionando classe de testes de sistema

--- src/principal/TesteDeSistema.java
+++ src/principal/TesteDeSistema.java
@@ -1,5 +1,22 @@
 package principal;
 
 public class TesteDeSistema {
-    
+    public static void main(String[] args) {
+        autenticar(false);
+    }
+
+    /**
+     * Analisa os dados de login do usuario e conferem com os dados cadastrados no
+     * sistema
+     * 
+     * @param verificacao
+     */
+    public static void autenticar(boolean verificacao) {
+        System.out.println("\n----------Teste autenticar--------");
+        if (verificacao) {
+            System.out.println("\nUsuário autenticado! Tem acesso ao sistema.");
+        } else {
+            System.out.println("\nUsuário não autenticado! Dados não conferem.");
+        }
+    }
 }
