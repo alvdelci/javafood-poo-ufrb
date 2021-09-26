e04b183299e800cc966c8842dcf257b317efe8be
alvdelci <delcimariosantos@gmail.com>
2021-09-25 22:02:11 -0300

    Adicionando funcionalidades ao programa

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -52,8 +52,8 @@ public class Principal {
         MenusUsuarios menuUser = new MenusUsuarios();
         // Instância de LoginUsuario
         LoginUsuario loginUser = new LoginUsuario();
-        //instancia de MenusServiços
-        MenusServicos produtServicos = new MenusServicos();
+        // instancia de MenusServicos
+        MenusServicos menuServicos = new MenusServicos();
 
         while (true) {
             int opcao = menu.exibirMenuInicial();
@@ -104,30 +104,63 @@ public class Principal {
 
                                 // imprimir toda a lista de produtos e serviços disponiveis
                                 case 1:
+                                    int categoriaCase1 = menuServicos.selecionarCategoria();
 
-                                    // Imprime todos os produtos de mercado cadastrados
-                                    for (ProdutoMercado element : produtoMercado) {
-                                        System.out.println(element.toString());
+                                    if (categoriaCase1 == 1) {
+                                        System.out.println("------------------ Catálogo de Produto ----------------\n");
+                                        for (ProdutoMercado element : produtoMercado) {
+                                            System.out.println(element.toString());
+                                        }
+                                    } else if (categoriaCase1 == 2) {
+                                        System.out
+                                                .println("-------------------Catálogo de Refeições ---------------\n");
+                                        for (Refeicao element : refeicao) {
+                                            System.out.println(element.toString());
+                                        }
+                                    } else if (categoriaCase1 == 3) {
+                                        System.out
+                                                .println("------------ Catálogo de Planos de Refeição -------------\n");
+                                        for (PlanoDeRefeicao element : planoDeRefeicao) {
+                                            System.out.println(element.toString());
+                                        }
                                     }
 
-                                    // Imprime todas as refeicoes cadastradas
-                                    for (Refeicao element : refeicao) {
-                                        System.out.println(element.toString());
-                                    }
-
-                                    // Imprime todos os planos de refeicao cadastrados
-                                    for (PlanoDeRefeicao element : planoDeRefeicao) {
-                                        System.out.println(element.toString());
-                                    }
                                     break;
 
                                 // ver item especifico pesquisando o id do produto
                                 case 2:
-                                    int x;
+                                    int categoriaCase2 = menuServicos.selecionarCategoria();
                                     try {
-                                        System.out.printf("qual o id do produto que vc quer ver: ");
-                                        x = input.nextInt();
-                                        System.out.println(produtoMercado.get(x));
+                                        if (categoriaCase2 == 1) {
+                                            String codigoDoProduto = menuServicos.selecionarProduto();
+                                            for (ProdutoMercado element : produtoMercado) {
+                                                if (element.getCodigo().equals(codigoDoProduto)) {
+                                                    System.out.println(
+                                                            "----------------- Detalhes do Produto -----------------\n");
+                                                    System.out.println(element);
+                                                }
+                                            }
+
+                                        } else if (categoriaCase2 == 2) {
+                                            String codigoDaRefeicao = menuServicos.selecionarProduto();
+                                            for (Refeicao element : refeicao) {
+                                                if (element.getCodigo().equals(codigoDaRefeicao)) {
+                                                    System.out.println(
+                                                            "------------------- Detalhes da Refeição -----------------\n");
+                                                    System.out.println(element);
+                                                }
+                                            }
+                                        } else if (categoriaCase2 == 3) {
+                                            String codigoDoPlano = menuServicos.selecionarProduto();
+                                            for (PlanoDeRefeicao element : planoDeRefeicao) {
+                                                if (element.getCodigo().equals(codigoDoPlano)) {
+                                                    System.out.println(
+                                                            "--------------- Detalhes do Plano de Refeição-------------\n");
+                                                    System.out.println(element);
+                                                }
+                                            }
+                                        }
+
                                     } catch (Exception e) {
                                         System.out.println("Caracter inserido não compatível!");
                                     }
@@ -170,25 +203,21 @@ public class Principal {
 
                                 // adicionar um anuncio de um produto de acordo com a categoria
                                 case 7:
-                                   
-                                    try {
-                                       
-                                        // adcionando anuncio
 
-                                        int opcaoCategoria = produtServicos.selecionarCategoria();
-                                        if (opcaoCategoria == 1) {
+                                    int categoriaCase7 = menuServicos.selecionarCategoria();
+                                    try {
+                                        if (categoriaCase7 == 1) {
                                             System.out.println("------- Cadastrar Produto ----------");
-                                            produtoMercado.add(produtServicos.cadastrarProduto());
-                                        } else if (opcaoCategoria == 2) {
+                                            produtoMercado.add(menuServicos.cadastrarProduto());
+                                        } else if (categoriaCase7 == 2) {
                                             System.out.println("------- Cadastrar Refeição ----------");
-                                            refeicao.add(produtServicos.cadastrarRefeicao());
-                                        }else if(opcaoCategoria==3){
+                                            refeicao.add(menuServicos.cadastrarRefeicao());
+                                        } else if (categoriaCase7 == 3) {
                                             System.out.println("------- Cadastrar Plano de Refeição ----------");
-                                            planoDeRefeicao.add(produtServicos.cadastrarPlanoDeRefeicao());
-                                        }else{
+                                            planoDeRefeicao.add(menuServicos.cadastrarPlanoDeRefeicao());
+                                        } else {
                                             System.out.println("Opção invalida.");
                                         }
-                                        
 
                                     } catch (Exception e) {
                                         System.out.println("Caracter inserido não compatível!");
@@ -204,33 +233,29 @@ public class Principal {
                                         System.out.println("Houve algum erro!!");
                                     }
 
-                                    //remover um anuncio com base no id dele
-                                     case 9:
-                                    
+                                    // remover um anuncio com base no id dele
+                                case 9:
+
+                                    int categoriaCase9 = menuServicos.selecionarCategoria();
                                     try {
-                                       
+
                                         // adcionando anuncio
 
-                                        int opcaoCategoria=produtServicos.selecionarCategoria();
-
-                                        if (opcaoCategoria == 1) {
+                                        if (categoriaCase9 == 1) {
                                             System.out.println("------- Remover Produto ----------");
-                                            for(){
-                                                
-                                            }
-                                            modelProdutoMercado.removerProduto(produtoR);;
-                                        } else if (opcaoCategoria == 2) {
+
+                                            // modelProdutoMercado.removerProduto();;
+                                        } else if (categoriaCase9 == 2) {
                                             System.out.println("------- Remover Refeição ----------");
 
-                                            modelRefeicao.removerRefeicao(removido);;
-                                        }else if(opcaoCategoria==3){
+                                            // modelRefeicao.removerRefeicao();;
+                                        } else if (categoriaCase9 == 3) {
                                             System.out.println("------- Remover Plano de Refeição ----------");
-                                            
-                                            modelPlanoDeRefeicao.removerPlanoDeRefeicao(pRemovido);;
-                                        }else{
+
+                                            // modelPlanoDeRefeicao.removerPlanoDeRefeicao();;
+                                        } else {
                                             System.out.println("Opção invalida.");
                                         }
-                                        
 
                                     } catch (Exception e) {
                                         System.out.println("Caracter inserido não compatível!");
